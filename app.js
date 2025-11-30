9b4fae87132623aa65e9b4c6caaf13551721499d
elijah <dataelijah@gmail.com>
2025-11-30 12:46:29 +0000

    feat: srcset, autoplay

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 function openImg(event) {
-  document.querySelector(".big-img").src = event.children[0].src;
+  document.getElementById("bigImg").src = event.children[0].src;
 }
 //this means that it takes the source from a child element and projects it over the parent element.
